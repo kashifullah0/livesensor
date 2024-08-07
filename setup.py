40fe6586271f4dc0a6ceec0b4c036325a0cfc6df
KashifUllah <kashifullah919@gmail.com>
2024-08-07 07:56:13 +0500

    after edding e

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements() -> List[str]:
+    requirements_list: List[str] = []
+    return requirements_list
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="kashif",
+    author_email="kashifullah919@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),  # ["pymongo"]
+)
